3f4e4e2023aa333bdcd42c394dc2b138cd7c71bc
Osvaldo Santana Neto <github-exemplo@osantana.me>
2024-03-20 20:33:18 -0300

    Adicionada atividade para a próxima aula

--- exercicio1.py
+++ exercicio1.py
@@ -4,7 +4,6 @@ arquivo = input("Informe o nome do arquivo CSV:")
 arquivo = open(arquivo)
 
 arquivo_csv = csv.reader(arquivo)
-print(arquivo_csv)
 
 print(f"{'Nome':40s} | {'RA':9s} | {'A1':3s} | {'A2':3s} | {'A3':3s} | {'Final':3s}")
 
@@ -23,3 +22,8 @@ for registro in arquivo_csv:
     nota = a1 + a2 + a3
 
     print(f"{nome:40s} | {ra:9s} | {a1:-3d} | {a2:-3d} | {a3:-3d} | {nota:-5d}")
+
+"""
+Atividade:
+Gerar o relatório agrupado por turma (turma == Curso + UC)
+"""
